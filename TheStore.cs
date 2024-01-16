dd254041c173182e58a4f58251e806a56d651479
Kurt Müllner <kurt.muellner@bluewin.ch>
2024-01-16 16:02:56 +0100

    asd

--- TheStore.cs
+++ TheStore.cs
@@ -7,7 +7,7 @@ namespace Odnalaz
     /// [S I M U L A T I O N E N]
     /// Odnalaz: Grosser, europaeischer online Kleider- und Schuhe Laden
     /// </summary>
-    class TheStore
+    public  class TheStore
     {
         /// <summary>
         /// Liste mit Produkten
@@ -18,21 +18,22 @@ namespace Odnalaz
         /// Haupteinstiegsfunktion
         /// </summary>
         /// <param name="args"></param>
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-           
             // Lager füllen
             Lager();
 
-            while(true)
+            while (true)
             {
-                switch(MenuAnzeigen())
+                switch (MenuAnzeigen())
                 {
                     case "a":
                         LagerAnzeigen();
                         break;
+
                     case "e":
                         return;
+
                     default:
                         break;
                 }
@@ -43,7 +44,7 @@ namespace Odnalaz
         /// Hauptmenu anzeigen
         /// </summary>
         /// <returns></returns>
-        static string MenuAnzeigen()
+        private static string MenuAnzeigen()
         {
             Console.WriteLine("------------------------");
             Console.WriteLine("ODNALAZ: Online-Shopping");
@@ -57,16 +58,16 @@ namespace Odnalaz
 
         /// <summary>
         /// Lager mit Stammdaten befüllen.
-        /// </summary>        
+        /// </summary>
         private static void Lager()
-        {           
+        {
             produkte.Add(new Produkt("Hose", 75f, ProdukteGruppe.BEKLEIDUNG, "M"));
             produkte.Add(new Produkt("T-Shirt", 45f, ProdukteGruppe.BEKLEIDUNG, "S"));
             produkte.Add(new Produkt("Adidas Rom", 180.75f, ProdukteGruppe.SCHUHE, "42"));
             produkte.Add(new Produkt("Halskette Schmudlinsky", 550.00f, ProdukteGruppe.SCHMUCK, "kurz"));
             produkte.Add(new Produkt("Ohringe 2 Stk. silber", 175.80f, ProdukteGruppe.SCHMUCK, "3 cm"));
             produkte.Add(new Produkt("Vans", 50.0f, ProdukteGruppe.SCHUHE, "47"));
-            produkte.Add(new Produkt("Redken Extreme", 28.50f,ProdukteGruppe.BEAUTY, "400ml"));
+            produkte.Add(new Produkt("Redken Extreme", 28.50f, ProdukteGruppe.BEAUTY, "400ml"));
             produkte.Add(new Produkt("Redken Soft", 18.50f, ProdukteGruppe.BEAUTY, "250ml"));
             produkte.Add(new Produkt("Schwarzkopf Black&Grey", 9.75f, ProdukteGruppe.BEAUTY, "200ml"));
         }
@@ -79,6 +80,7 @@ namespace Odnalaz
             Console.WriteLine("Filterung nach Produktegruppen");
             Console.WriteLine("Kleidung  : k");
             Console.WriteLine("Schuhe    : s");
+            Console.WriteLine("Kleider und Schuhe   : ks");
             Console.WriteLine("Schmuck   : m");
             Console.WriteLine("Beauty    : b");
             Console.WriteLine("Alle      : a");
@@ -87,8 +89,9 @@ namespace Odnalaz
 
             ProdukteGruppe grp = ProdukteGruppe.NONE;
 
-            switch(sAuswahl)
+            switch (sAuswahl)
             {
+                case "ks":grp = ProdukteGruppe.BEKLEIDUNG | ProdukteGruppe.SCHUHE; break;
                 case "k": grp = ProdukteGruppe.BEKLEIDUNG; break;
                 case "s": grp = ProdukteGruppe.SCHUHE; break;
                 case "m": grp = ProdukteGruppe.SCHMUCK; break;
@@ -96,16 +99,28 @@ namespace Odnalaz
                 case "a": grp = ProdukteGruppe.NONE; break;
                 default: break;
             }
-                
+
             foreach (var p in produkte)
             {
                 // Filterung nach Produktegruppe
-                if ( p.Gruppe == grp || grp == ProdukteGruppe.NONE)
+                if (ProdukteGruppe.NONE == grp)
                 {
                     Console.WriteLine(p.Name + ", " + p.Preis + ", " + p.Gruppe);
                 }
+                else if (p.Gruppe.HasFlag(grp))
+                {
+                    Console.WriteLine(p.Name + ", " + p.Preis + ", " + p.Gruppe);
+                }
+                else if ( grp.HasFlag(ProdukteGruppe.BEKLEIDUNG | ProdukteGruppe.SCHUHE) )
+                {
+                    if (p.Gruppe == ProdukteGruppe.BEKLEIDUNG || p.Gruppe == ProdukteGruppe.SCHUHE)
+                    Console.WriteLine(p.Name + ", " + p.Preis + ", " + p.Gruppe);
+                }               
+                else
+                {
+                    // neue Produktegruppe oder Gruppierung muesste hier abgehandelt werden
+                }
             }
-
         }
     }
 }
